0f6c63d6dc92b8e52c30232a5b3744cadd38a259
surface_pro_okazia <compresce+surfacepro@gmail.com>
2019-12-13 21:29:53 -0800

    bootstrap

--- demo-project/src/index.js
+++ demo-project/src/index.js
@@ -5,7 +5,32 @@ const MainSection = () => {
   return (
     <div className="container">
       <div className="text-center">
-        Our first example with React. Really!
+          <ul className="list-group mb-3">
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <h6 className="my-0">First item</h6>
+              <small class="text-muted">Some description</small>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <h6 className="my-0">Second item</h6>
+              <small class="text-muted">Some description</small>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <h6 className="my-0">Third item</h6>
+              <small class="text-muted">Some description</small>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <h6 className="my-0">Fourth item</h6>
+              <small class="text-muted">Some description</small>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <h6 className="my-0">Fifth item</h6>
+              <small class="text-muted">Some description</small>
+            </li>
+            <li className="list-group-item d-flex justify-content-between lh-condensed">
+              <h6 className="my-0">Sixth item</h6>
+              <small class="text-muted">Some description</small>
+            </li>
+          </ul>
       </div>
     </div>
   )
